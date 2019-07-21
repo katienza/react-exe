1704a5b6fc7f788e39decb91851e6b3b4b480aa6
Ken Atienza <atienza.ken@gmail.com>
2019-07-21 15:29:24 -0700

    Completed 01.4 exercise.

--- exercises/01.4-building-a-layout/index.js
+++ exercises/01.4-building-a-layout/index.js
@@ -1,20 +1,38 @@
-import React from "react"; //Main React.js library
-import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
+import React from 'react'; //Main React.js library
+import ReactDOM from 'react-dom'; //we use ReactDOM to render into the DOM
 
 const data = {
-	image: "https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/",
-	cardTitle: "Bob Dylan",
-	cardDescription:
-		"Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
-	button: {
-		url: "https://en.wikipedia.org/wiki/Bob_Dylan",
-		label: "Go to wikipedia"
-	}
+  image:
+    'https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/',
+  cardTitle: 'Bob Dylan',
+  cardDescription:
+    'Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.',
+  button: {
+    url: 'https://en.wikipedia.org/wiki/Bob_Dylan',
+    label: 'Go to wikipedia',
+  },
 };
 
 /**
  * define the variable 'content' here and fill it with the
  * needed code to render the bootstrap card
  **/
-
-ReactDOM.render(content, document.querySelector("#myDiv"));
+const content = (
+  <div class='card m-5'>
+    <img
+      class='card-img-top'
+      src={data.image}
+      alt='Card image cap'
+    />
+    <div class='card-body'>
+      <h5 class='card-title'>{data.cardTitle}</h5>
+      <p class='card-text'>
+		{data.cardDescription}
+      </p>
+      <a href={data.button.url} class='btn btn-primary'>
+        {data.button.label}
+      </a>
+    </div>
+  </div>
+);
+ReactDOM.render(content, document.querySelector('#myDiv'));
