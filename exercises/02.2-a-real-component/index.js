67e272b1cf44654c6b93eaaf159bd426efb559f1
Ken Atienza <atienza.ken@gmail.com>
2019-07-27 10:28:47 -0700

    Completed 02.2 exercise.

--- exercises/02.2-a-real-component/index.js
+++ exercises/02.2-a-real-component/index.js
@@ -1,6 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 //create your function here
-
+const BootstrapCard = () => {
+  <div class='card m-5'>
+    <img
+      class='card-img-top'
+      src='https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/'
+      alt='Card image cap'
+    />
+    <div class='card-body'>
+      <h5 class='card-title'>Bob Dylan</h5>
+      <p class='card-text'>
+        Bob Dylan (born Robert Allen Zimmerman, May 24,
+        1941) is an American singer/songwriter, author, and
+        artist who has been an influential figure in popular
+        music and culture for more than five decades.
+      </p>
+      <a
+        href='https://en.wikipedia.org/wiki/Bob_Dylan'
+        class='btn btn-primary'>
+        Go to wikipedia
+      </a>
+    </div>
+  </div>;
+};
 //remember to use ReactDOM.render to include the component into the website
+
+ReactDOM.render(
+  <BootstrapCard />,
+  document.getElementById('myDiv'),
+);
